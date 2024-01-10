0082456a662fdd75d9998c20713f0fbc4fc92b8b
MuhDavin213 <davin.new58@gmail.com>
2024-01-10 17:42:34 +0700

    add PenyediaViewModel init homeviewmodel , add homeviewmodel serta membuat bebrapa class

---
+++ app/src/main/java/com/example/theelektronik/ui/PenyediaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.theelektronik.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.theelektronik.ProdukApplication
+import com.example.theelektronik.ui.home.HomeViewModel
+
+fun CreationExtras.aplikasiProduk():ProdukApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ProdukApplication)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiProduk().container.produkRepositori)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/theelektronik/ui/home/HomeViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.theelektronik.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.theelektronik.data.ProdukRepositori
+import com.example.theelektronik.model.Produk
+import com.example.theelektronik.ui.HomeUIState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+sealed class ProdukUIState {
+    data class Success(val produk: Flow<List<Produk>>) : ProdukUIState()
+    object Error : ProdukUIState()
+    object Loading : ProdukUIState()
+}
+
+class HomeViewModel(private val produkRepositori: ProdukRepositori) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUIState: StateFlow<HomeUIState> = produkRepositori.getAll()
+        .filterNotNull()
+        .map {
+            HomeUIState (listProduk = it.toList(), it.size ) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUIState()
+
+        )
+
+}
